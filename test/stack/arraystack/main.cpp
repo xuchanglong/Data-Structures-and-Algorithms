fe4adae51f93ee5c1c6dc27d6c76ec5f2616546b
xuchanglong <wwwitworld@sina.com>
2019-07-29 05:55:29 +0100

    Modify function name and comments.

--- test/stack/arraystack/main.cpp
+++ test/stack/arraystack/main.cpp
@@ -1,22 +1,37 @@
 #include "arraystack.hpp"
 #include <iostream>
- 
+
+/**
+ * @function    依次将元素弹出栈，显示到控制台上。
+ * @paras       stack   栈
+ * @return      none。
+ */
 template <typename T>
 void showstack(arraystack<T> &stack);
 
+/**
+ * @function    针对不同存储类型的栈，进行测试。
+ * @paras       stacksize   栈大小。
+ *              pdata       待测试的数组。
+ *              datasize    数组大小。
+ * @return      测试结果。
+ */
 template <typename T>
-int showinttype(const int & stacksize , const T * const pdata , const int & datasize);
- 
+int teststack(const int &stacksize, const T *const pdata, const int &datasize);
+
 int main()
 {
     int pdata[10] = {1, 43, 4, 3, 2, 9, 35, 654, 65, 732};
-    showinttype(5 , pdata , 10);
+    teststack(5, pdata, 10);
 
-    double dsum[7] = {1.62 , 89.854 , 42.0254 , 57.9513 , 726.8913 , 71345 , 5251.54};
-    showinttype(5 , dsum , 7);
+    std::cout << std::endl;
+
+    double dsum[7] = {1.62, 89.854, 42.0254, 57.9513, 726.8913, 71345, 5251.54};
+    teststack(5, dsum, 7);
+
     return 0;
 }
- 
+
 template <typename T>
 void showstack(arraystack<T> &stack)
 {
@@ -28,8 +43,8 @@ void showstack(arraystack<T> &stack)
     std::cout << std::endl;
 }
 
-template<typename T>
-int showinttype(const int & stacksize , const T * const pdata , const int & datasize)
+template <typename T>
+int teststack(const int &stacksize, const T *const pdata, const int &datasize)
 {
     arraystack<T> stack;
     if (stack.create(stacksize) != 0)
@@ -48,7 +63,7 @@ int showinttype(const int & stacksize , const T * const pdata , const int & data
         }
     }
     showstack(stack);
- 
+
     std::cout << "-------------------  push_c  -------------------" << std::endl;
     //  若数据溢出，则重新申请内存，继续压数。
     for (size_t i = 0; i < datasize; i++)
