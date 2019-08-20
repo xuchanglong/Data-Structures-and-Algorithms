086e3695e7a52ad610ef6a9abefb663c7068b929
xuchanglong <wwwitworld@sina.com>
2019-08-20 17:43:53 +0800

    Add selectsort algorithms and test .

--- algorithms/sorts/sorts.hpp
+++ algorithms/sorts/sorts.hpp
@@ -5,6 +5,13 @@
 */
 #include <cstddef>
 
+#define SWAP(x, y)  \
+    {               \
+        (x) ^= (y); \
+        (y) ^= (x); \
+        (x) ^= (y); \
+    }
+
 /**
  * @function    插入排序算法。
  * @paras       pdata   待排序的数据的首地址。
@@ -34,6 +41,42 @@ int InsertSort(T *pdata, size_t count)
             k--;
         }
         pdata[k + 1] = tmp;
+    }
+
+    return 0;
+}
+
+/**
+ * @function    选择排序算法。
+ * @paras       pdata   待排序的数据的首地址。
+ *              sum     待排序的数据的数量。
+ * @return      0   操作成功。
+ *              -1  pdata = nullptr
+ * @author      xuchanglong
+ * @time        2019-08-20
+*/
+template <typename T>
+int SelectSort(T *pdata, size_t count)
+{
+    int imin = 0;
+    if (pdata == nullptr)
+    {
+        return -1;
+    }
+    for (size_t i = 0; i < count - 1; i++)
+    {
+        imin = i;
+        for (size_t k = i + 1; k < count; k++)
+        {
+            if (pdata[k] < pdata[imin])
+            {
+                imin = k;
+            }
+        }
+        if (i != imin)
+        {
+            SWAP(pdata[i], pdata[imin]);
+        }
     }
 
     return 0;
--- test/algorithms/sorts/main.cc
+++ test/algorithms/sorts/main.cc
@@ -4,6 +4,12 @@
 template <typename T>
 void Show(T *pdata, size_t count);
 
+/**
+ * 1    插入排序。
+ * 2    选择排序。
+*/
+#define SORTALGORITHMSTYPE 2
+
 int main()
 {
     int testnum[10] = {1, 2, 3, 4, 5, 2, 6, 4, 8, 4};
@@ -11,7 +17,19 @@ int main()
     std::cout << "原始数据。" << std::endl;
     Show(testnum, 10);
 
-    InsertSort(testnum, 10);
+    switch (SORTALGORITHMSTYPE)
+    {
+    case 1:
+        std::cout << "插入排序，";
+        InsertSort(testnum, 10);
+        break;
+    case 2:
+        std::cout << "选择排序，";
+        SelectSort(testnum, 10);
+        break;
+    default:
+        break;
+    }
 
     std::cout << "排序之后的数据。" << std::endl;
     Show(testnum, 10);
