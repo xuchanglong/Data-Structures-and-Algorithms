b95e6781d4da813a1a02b7b6f6f9102429ecf8f4
xuchanglong <wwwitworld@sina.com>
2019-08-25 16:14:15 +0800

    Inited merge sort .

--- algorithms/sorts/sorts.hpp
+++ algorithms/sorts/sorts.hpp
@@ -4,6 +4,7 @@
  * @time        2019-08-20
 */
 #include <cstddef>
+#include <string.h>
 
 #define SWAP(x, y)  \
     {               \
@@ -11,13 +12,13 @@
         (y) ^= (x); \
         (x) ^= (y); \
     }
-
+/***************************************************  插入排序  ***************************************************/
 /**
  * @function    插入排序算法。
  * @paras       pdata   待排序的数据的首地址。
- *              count   待排序的数据的数量。
+ *                      count   待排序的数据的数量。
  * @return      0   操作成功。
- *              -1  pdata = nullptr
+ *                      -1  pdata = nullptr
  * @author      xuchanglong
  * @time        2019-08-20
 */
@@ -28,7 +29,6 @@ int InsertSort(T *pdata, size_t count)
     {
         return -1;
     }
-    int i = 0;
     int k = 0;
     T tmp;
     for (size_t i = 1; i < count; i++)
@@ -45,20 +45,20 @@ int InsertSort(T *pdata, size_t count)
 
     return 0;
 }
-
+/***************************************************  选择排序  ***************************************************/
 /**
  * @function    选择排序算法。
  * @paras       pdata   待排序的数据的首地址。
- *              count   待排序的数据的数量。
+ *                      count   待排序的数据的数量。
  * @return      0   操作成功。
- *              -1  pdata = nullptr
+ *                      -1  pdata = nullptr
  * @author      xuchanglong
  * @time        2019-08-20
 */
 template <typename T>
 int SelectSort(T *pdata, size_t count)
 {
-    int imin = 0;
+    size_t imin = 0;
     if (pdata == nullptr)
     {
         return -1;
@@ -81,24 +81,60 @@ int SelectSort(T *pdata, size_t count)
 
     return 0;
 }
-
+/***************************************************  归并排序  ***************************************************/
 /**
  * @function    归并排序算法的合并函数。
- * @paras       pdata   待排序的数据的首地址。
- *              s       待排序的数据的起始位置。
- *              e       待排序的数据的结束位置。
- * @return      0   操作成功。
- *              -1  pdata = nullptr 。
+ * @paras          pdata    待排序的数据的首地址。
+ *                          s               待排序的数据的起始位置。
+ *                          m             待排序的数据的中间位置。
+ *                          e              待排序的数据的结束位置。
+ * @return        0     操作成功。
+ *                          -1   pdata = nullptr 。
  * @author      xuchanglong
- * @time        2019-08-22
+ * @time          2019-08-22
 */
 template <typename T>
-int _MergeSort(T *pdata, size_t s, size_t tmp, size_t e)
+int _Merge(T *pdata, const size_t &s, const size_t &m, const size_t &e)
 {
     if (pdata == nullptr)
     {
         return -1;
     }
+    T *ptmp = new T[e - s + 1];
+    size_t i, t, k;
+    for (i = s, t = m + 1, k = 0; (i <= m) && (t <= e);)
+    {
+        if (pdata[i] <= pdata[t])
+        {
+            ptmp[k++] = pdata[i++];
+        }
+        else
+        {
+            ptmp[k++] = pdata[t++];
+        }
+    }
+    /**
+     * 将剩余的数据补充到排序好的数组中。
+    */
+    if (i == m + 1)
+    {
+        while (t <= e)
+        {
+            ptmp[k++] = pdata[t++];
+        }
+    }
+    else
+    {
+        while (i <= m)
+        {
+            ptmp[k++] = pdata[i++];
+        }
+    }
+
+    memcpy(pdata + s, ptmp, sizeof(T) * (e - s + 1));
+    delete[] ptmp;
+    ptmp = nullptr;
+    return 0;
 }
 
 /**
@@ -113,19 +149,20 @@ int _MergeSort(T *pdata, size_t s, size_t tmp, size_t e)
  * @time        2019-08-22
 */
 template <typename T>
-int _MergeSort(T *pdata, size_t s, size_t e)
+int _MergeSort(T *pdata, const size_t &s, const size_t &e)
 {
-    size_t tmp = (s + e) / 2;
+    const size_t m = (s + e) >> 1;
     if (pdata == nullptr)
     {
         return -1;
     }
-    if (s <= e)
+    if (s >= e)
     {
         return -2;
     }
-    _MergeSort(pdata, s, tmp);
-    _MergeSort(pdata, tmp, e);
+    _MergeSort(pdata, s, m);
+    _MergeSort(pdata, m, e);
+    _Merge(pdata, s, m, e);
     return 0;
 }
 
@@ -139,12 +176,12 @@ int _MergeSort(T *pdata, size_t s, size_t e)
  * @time        2019-08-22
 */
 template <typename T>
-int MergeSort(T *pdata, size_t count)
+int MergeSort(T *pdata, const size_t &count)
 {
     if (pdata == nullptr)
     {
         return -1;
     }
-    _MergeSort(pdata, 0, count);
+    _MergeSort(pdata, 0, count - 1);
     return 0;
 }
--- test/algorithms/sorts/main.cc
+++ test/algorithms/sorts/main.cc
@@ -1,23 +1,24 @@
 #include "sorts/sorts.hpp"
 #include <iostream>
+#include <cstdio>
 
 template <typename T>
 void Show(T *pdata, size_t count);
 
-/**
- * 1    插入排序。
- * 2    选择排序。
-*/
-#define SORTALGORITHMSTYPE 2
-
-int main()
+    /**
+    * 1    插入排序。
+    * 2    选择排序。
+    * 3    归并排序。
+    */
+int main(int argc, const char *argv[])
 {
-    int testnum[10] = {1, 2, 3, 4, 5, 2, 6, 4, 8, 4};
+    int algtype = atoi(argv[1]);
+    int testnum[10] = {13, 17, 3, 4, 5, 2, 6, 4, 8, 4};
 
     std::cout << "原始数据。" << std::endl;
     Show(testnum, 10);
 
-    switch (SORTALGORITHMSTYPE)
+    switch (algtype)
     {
     case 1:
         std::cout << "插入排序，";
@@ -26,6 +27,10 @@ int main()
     case 2:
         std::cout << "选择排序，";
         SelectSort(testnum, 10);
+        break;
+    case 3:
+        std::cout << "归并排序，";
+        MergeSort(testnum, 10);
         break;
     default:
         break;
